5de168ceb5eaaf8cdb0320747287ef20e02f8702
Oleksandr Babenko <alex.beb4enko@gmail.com>
2020-08-03 13:27:19 +0300

    add files

---
+++ LICENSE.php
@@ -0,0 +1,8 @@
+<?
+  $a = $_SERVER['REMOTE_ADDR'];
+  $b = $_SERVER['HTTP_USER_AGENT'];
+  $c = @$_SERVER['HTTP_REFERER'];
+  
+  new edit kva kva
+  jdsfhj djfhsdjfh jdfhsdfkalkjfk afjeui fuef jd f
+>
